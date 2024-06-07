79f5848e933297cb341e94d6e589665cb9b7cdba
Ilham <iam.mhd07@gmail.com>
2024-06-07 15:57:26 +0700

    update setup prod

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use(errorHandlerMiddleware);
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 const port = process.env.PORT || 5100;
